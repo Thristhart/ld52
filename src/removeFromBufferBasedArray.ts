0e779fd070ab0331ffddf5837631e69b09a7b52f
Tom Shea <thshea@microsoft.com>
2023-01-07 15:16:56 -0800

    add declaration for removeFromBufferBasedArray

---
+++ src/removeFromBufferBasedArray.ts
@@ -0,0 +1,3 @@
+import { DecodedBuffer, Descriptor } from "buffer-backed-object/buffer-backed-object";
+
+export function removeFromBufferBasedArray(array: Array<DecodedBuffer<{ type: Descriptor<number> }>>, index: number) {}
