88c7d18c2a1658dd2888cfa697d76f65f5f4f52a
Tom Shea <thshea@microsoft.com>
2023-01-08 13:15:57 -0800

    add place tower shop will go, change bg color with season

--- src/render/drawUI.ts
+++ src/render/drawUI.ts
@@ -4,4 +4,5 @@ const healthbar = document.getElementById("healthbar") as HTMLProgressElement;
 
 export function drawUI(state: GameState) {
     healthbar.value = state.playerHealth;
+    document.body.dataset.season = state.season.toString();
 }
