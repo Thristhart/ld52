0508f25d97b2dc5b913d9bff4513b2ea3d732dfe
Tom Shea <thshea@microsoft.com>
2023-01-08 14:10:26 -0800

    add sentry integration for fun

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import { startMusicForSeason } from "./bgMusic";
 import { Season } from "./models/season";
 import { startLoop } from "./render/renderLoop";
 
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://7f0991112fe64ae6a3536fc109c8c1d5@o4504471170842624.ingest.sentry.io/4504471173857280",
+    integrations: [new BrowserTracing()],
+
+    tracesSampleRate: 1.0,
+});
+
 setupListeners();
 startLoop();
 startMusicForSeason(Season.Spring);
